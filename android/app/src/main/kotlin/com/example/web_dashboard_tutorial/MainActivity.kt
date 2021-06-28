91c428ab371f2745a4b17db0b37f7bf0d03aeff2
Omkar2620 <otbhostekar@gmail.com>
2021-06-29 01:05:34 +0530

    First Commit: 58M Completed

---
+++ android/app/src/main/kotlin/com/example/web_dashboard_tutorial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_dashboard_tutorial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/layout.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:web_dashboard_tutorial/helpers/responsiveness.dart';
+import 'package:web_dashboard_tutorial/widgets/large_screen.dart';
+import 'package:web_dashboard_tutorial/widgets/small_screen.dart';
+import 'package:web_dashboard_tutorial/widgets/top_nav.dart';
+
+class SiteLayout extends StatelessWidget {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: topNavigationBar(context, scaffoldKey),
+      body: ResponsiveWidget(
+        largeScreen: LargeScreen(),
+        smallScreen: SmallScreen(),
+      ),
+    );
+  }
+}
